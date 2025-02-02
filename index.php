4c52084bb4fee548b720a788a2876deb332fd400
momotan039 <momotaha039@gmail.com>
2025-02-02 17:27:41 +0200

    combine the sections into index file

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php get_header() ?>
+<?php get_template_part('parts/services')?>
+<?php get_template_part('parts/business-plan')?>
+<?php get_template_part('parts/features')?>
+<?php get_template_part('parts/showcase')?>
+<?php get_template_part('parts/pricing')?>
+<?php get_template_part('parts/testimonials')?>
+<?php get_template_part('parts/team')?>
+<?php get_template_part('parts/blog')?>
+<?php get_template_part('parts/contact-us')?>
+<?php get_footer() ?>
